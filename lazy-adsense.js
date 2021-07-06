7b3e087c6549d586c9a3fdb97454c14c5f2543ca
UThemes <86716632+ulathemes@users.noreply.github.com>
2021-07-06 18:25:52 +0530

    Lazyload Adsense
    [Close Body] Lazyload Adsense for ulathemes

---
+++ lazy-adsense.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'>
+//<![CDATA[
+var lazyadsense2 = false;
+window.addEventListener("scroll", function(){
+if ((document.documentElement.scrollTop != 0 && lazyadsense2 === false) || (document.body.scrollTop != 0 && lazyadsense2 === false)) {
+(function() { var ad = document.createElement('script'); ad.setAttribute('data-ad-client','ca-pub-XXXXXXXXXXX'); ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+lazyadsense2 = true;
+  }
+}, true);
+//]]>
+</script>
